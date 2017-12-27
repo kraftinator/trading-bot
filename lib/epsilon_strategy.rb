a124d55e88aa1edc23db0e3b3e6702df57b0d1d8
Adam Kraft <adamkraft1@gmail.com>
2017-12-27 10:26:16 -0800

    updated Epsilon strategy

--- lib/epsilon_strategy.rb
+++ lib/epsilon_strategy.rb
@@ -1,7 +1,25 @@
 require './lib/trading_strategy.rb' 
 
 class EpsilonStrategy < TradingStrategy
+    
+  def buy_order_limit_price
+    ## Set limit price to last price if it's not 10% higher than weighted avg price.
+    ## This is a more conservative type of Gamma bot.
+    if @tps['last_price'] > ( @tps['weighted_avg_price'] * 1.1 )
+      #limit_price = @tps['weighted_avg_price'] * 1.1
+      limit_price = @tps['weighted_avg_price']
+    else
+      limit_price = @tps['last_price']
+    end
+    ## Get target limit price based on percentage range.
+    limit_price = limit_price * ( 1 - @trader.percentage_range.to_f )
+    ## Add precision to limit price. API will reject if too long.
+    limit_price = limit_price.round( @precision )
+    limit_price
+  end
   
+  
+=begin
   def buy_order_limit_price
     ## Choose last price or weighted avg price, whichever is less.
     limit_price = ( @tps['last_price'] < @tps['weighted_avg_price'] ) ? @tps['last_price'] : @tps['weighted_avg_price']
@@ -11,5 +29,6 @@ class EpsilonStrategy < TradingStrategy
     limit_price = limit_price.round( @precision )
     limit_price
   end
-  
+=end
+    
 end
