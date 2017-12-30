f66e0016d3180a4a9eda4ba5b1428b4d00258f30
Adam Kraft <adamkraft1@gmail.com>
2017-12-30 12:22:10 -0800

    removed high price check from Epsilon strategy

--- lib/epsilon_strategy.rb
+++ lib/epsilon_strategy.rb
@@ -5,7 +5,7 @@ class EpsilonStrategy < TradingStrategy
   def buy_order_limit_price
     ## Set limit price to last price if it's not 10% > weighted avg price and high price is not 10% > weighted avg price.
     ## This is a more conservative type of Gamma bot.
-    if ( @tps['last_price'] > ( @tps['weighted_avg_price'] * 1.1 ) ) or ( @tps['high_price'] > ( @tps['weighted_avg_price'] * 1.1 ) )
+    if ( @tps['last_price'] > ( @tps['weighted_avg_price'] * 1.1 ) ) #or ( @tps['high_price'] > ( @tps['weighted_avg_price'] * 1.1 ) )
       #limit_price = @tps['weighted_avg_price']
       limit_price = ( @tps['last_price'] < @tps['weighted_avg_price'] ) ? @tps['last_price'] : @tps['weighted_avg_price']
     else
