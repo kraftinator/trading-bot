8d8c566fb785894d1417c4b6f6f278518dfae8a9
Adam Kraft <adamkraft1@gmail.com>
2018-01-17 16:45:43 -0600

    added traders table to account index page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,6 +4,7 @@ class AccountsController < ApplicationController
   before_action :active_required, :except => [:inactive]
   
   def index
+    @traders = current_user.traders.all
   end
   
   def inactive
