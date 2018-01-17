83596ec9c473c546b8df1dbb0eda7035c3c5868f
Adam Kraft <adamkraft1@gmail.com>
2018-01-17 16:50:28 -0600

    Only retrieve active traders

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,7 +4,7 @@ class AccountsController < ApplicationController
   before_action :active_required, :except => [:inactive]
   
   def index
-    @traders = current_user.traders.all
+    @traders = current_user.traders.where( active: true ).to_a
   end
   
   def inactive
