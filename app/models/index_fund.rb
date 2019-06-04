5f39208bd0e74ec300544fdf5748ed845ac77fc4
Adam Kraft <adamkraft1@gmail.com>
2019-06-04 13:37:06 -0500

    Improved Edit Fund UX

--- app/models/index_fund.rb
+++ app/models/index_fund.rb
@@ -7,8 +7,22 @@ class IndexFund < ApplicationRecord
   
   delegate  :exchange, :to => :base_coin
   
+  validates :rebalance_trigger_pct, numericality: { greater_than: 0 }
+  
+  before_validation(on: [:create, :update]) do
+    self.rebalance_trigger_pct = rebalance_trigger_pct.to_f/100.to_f
+  end
+  
   def allocations
     self.index_fund_coins
+  end
+  
+  def allocation_pct_total
+    self.index_fund_coins.sum(:allocation_pct)
+  end
+  
+  def allocations_valid?
+    allocation_pct_total == 1.0
   end
   
 end
